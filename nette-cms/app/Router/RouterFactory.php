9c559c2972d702613bf4e122adc430b9fa615639
Purchart Jan <purchart@techart.cz>
2022-04-06 12:38:39 +0200

    zobrazeni clanku

--- nette-cms/app/Router/RouterFactory.php
+++ nette-cms/app/Router/RouterFactory.php
@@ -15,7 +15,13 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router[] = $module = new RouteList('Admin');
+		$module->addRoute('admin/<presenter>/<action>', 'Dashboard:default');
+
+		$router[] = $module = new RouteList('Front');
+		$module->addRoute('<presenter>/<action>', 'Homepage:default');
+		
 		return $router;
+
 	}
 }
