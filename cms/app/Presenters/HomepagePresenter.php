eff57518cd1ba0f02f184e995656e38b6a360052
Purchart Jan <purchart@techart.cz>
2022-05-01 08:33:45 +0200

    connect to database and loading articles

--- cms/app/Presenters/HomepagePresenter.php
+++ cms/app/Presenters/HomepagePresenter.php
@@ -5,8 +5,23 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Presenter;
+use Nette\Database\Context;
 
-
-final class HomepagePresenter extends Nette\Application\UI\Presenter
+final class HomepagePresenter extends Presenter
 {
+    /** Nette\Database\Context */
+    private $database;
+
+    public function __construct(Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->articles = $this->database->table('article')
+            ->order('date_add DESC')
+            ->limit(5);
+    }
 }
